e925a9aeabb4d125e462eb94d633b6c93e75c7f3
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-23 16:32:11 +1000

    Basic Register form - no validation or post request

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { FaUser } from 'react-icons/fa'
 
 function Register() {
+  const [formData, setFormData] = useState( { 
+    username: '',
+    email: '',
+    familyname: '',
+    password: '',
+    passwordconfirm: ''
+  });
+
+  const { username, email, familyname, password, passwordconfirm } = formData;  // destructuring for easy access in form
+
+  const onChange = (e) => {
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.name] : e.target.value
+    }));
+  };
+
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
-    <div>
-        <h1>Register page</h1>
-    </div>
+    <>
+        <section className="heading">
+          <h1>
+            <FaUser /> Register your family here...
+          </h1>
+          <p>Please create an account</p>
+        </section>
+
+        <div className="form">
+          <form onSubmit={ onSubmit }>
+            <div className="form-group">
+              <label>
+              Username:
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  id="username" 
+                  name="username"
+                  value={ username } 
+                  placeholder="Enter a username"
+                  onChange={ onChange } 
+                />
+              </label>
+            </div>
+
+            <div className="form-group">
+              <label>
+              Email:
+                <input 
+                  type="email" 
+                  className="form-control" 
+                  id="email" 
+                  name="email"
+                  value={ email } 
+                  placeholder="Enter your email address"
+                  onChange={ onChange } 
+                />
+              </label>
+            </div>
+            
+            <div className="form-group">
+                <label>
+                Family Name:
+                  <input 
+                    type="text" 
+                    className="form-control" 
+                    id="familyname" 
+                    name="familyname"
+                    value={ familyname } 
+                    placeholder="Enter a family name e.g. The Bloggs Family"
+                    onChange={ onChange } 
+                  />
+                </label>
+              </div>
+
+              <div className="form-group">
+                <label>
+                Password:
+                  <input 
+                    type="password" 
+                    className="form-control" 
+                    id="password" 
+                    name="password"
+                    value={ password } 
+                    placeholder="Enter a password"
+                    onChange={ onChange } 
+                  />
+                </label>
+              </div>
+
+              <div className="form-group">
+                <label>
+                Password Confirmation:
+                  <input 
+                    type="password" 
+                    className="form-control" 
+                    id="passwordconfirm" 
+                    name="passwordconfirm"
+                    value={ passwordconfirm } 
+                    placeholder="Confirm your password"
+                    onChange={ onChange } 
+                  />
+                </label>
+              </div>
+
+              <div className="form-group">
+                <button className="btn btn-block">Submit</button>
+              </div>
+            </form>
+        </div>
+    </>
   )
 }
 

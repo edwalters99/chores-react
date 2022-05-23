94e8b44bde9481469510ffef5dd00d290a7d759e
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-23 22:58:39 +1000

    homepage started - pleceholder text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaQuestionCircle } from 'react-icons/fa'
 
 function Home() {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <>
+        <section className="heading">
+          <h1>Welcome to ChoreTracker</h1>
+          <p>Parent Dashboard</p>
+          <p>Display childlist if exists</p>
+          <p>Child 1</p>
+          <p>Child 2</p>
+          <p>Child 3</p>
+          <p>OR</p>
+         
+        </section>
+
+        <Link to='/new-child' className="btn btn-reverse btn-block">
+          <FaQuestionCircle />  Add Child
+        </Link>
+    </>
   )
 }
 
